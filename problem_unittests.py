94e874466a06b9433d2bc3e57fafe5ecda3da30e
Abhishek Tripathi <abhishek3.6@gmail.com>
2021-01-22 05:28:26 +0000

    Defined forward and backprop methods.Trained model for 4 epochs Generated a TV script from the trained model

--- problem_unittests.py
+++ problem_unittests.py
@@ -283,4 +283,43 @@ def test_rnn_gru(RNN, train_on_gpu):
     assert_test.test(assert_condition, assert_message)
     
     _print_success_message()
+    
+def test_forward_back_prop_gru(RNN, forward_back_prop, train_on_gpu):
+    batch_size = 200
+    input_size = 20
+    output_size = 10
+    sequence_length = 3
+    embedding_dim=15
+    hidden_dim = 10
+    n_layers = 2
+    learning_rate = 0.01
+    
+    # create test RNN
+    rnn = RNN(input_size, output_size, embedding_dim, hidden_dim, n_layers)
+    
+    mock_decoder = MagicMock(wraps=_TestNN(input_size, output_size))
+    if train_on_gpu:
+        mock_decoder.cuda()
+    
+    mock_decoder_optimizer = MagicMock(wraps=torch.optim.Adam(mock_decoder.parameters(), lr=learning_rate))
+    mock_criterion = MagicMock(wraps=torch.nn.CrossEntropyLoss())
+    
+    with patch.object(torch.autograd, 'backward', wraps=torch.autograd.backward) as mock_autograd_backward:
+        inp = torch.FloatTensor(np.random.rand(batch_size, input_size))
+        target = torch.LongTensor(np.random.randint(output_size, size=batch_size))
+        
+        hidden = rnn.init_hidden(batch_size)
+        
+        loss, hidden_out = forward_back_prop(mock_decoder, mock_decoder_optimizer, mock_criterion, inp, target, hidden)
+    
+    assert (hidden_out[0]==hidden[0]).sum()==batch_size*hidden_dim, 'Returned hidden state is the incorrect size.'
+    
+    assert mock_decoder.zero_grad.called or mock_decoder_optimizer.zero_grad.called, 'Didn\'t set the gradients to 0.'
+    assert mock_decoder.forward_called, 'Forward propagation not called.'
+    assert mock_autograd_backward.called, 'Backward propagation not called'
+    assert mock_decoder_optimizer.step.called, 'Optimization step not performed'
+    assert type(loss) == float, 'Wrong return type. Expected {}, got {}'.format(float, type(loss))
+    
+    _print_success_message()
+        
 
